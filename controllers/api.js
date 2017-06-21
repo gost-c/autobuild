5d8b7e9e54e27684a417f0e38b2efabc7a3ddbea
zcong1993 <1005997919@qq.com>
2017-06-21 10:49:46 +0800

    not wait

--- controllers/api.js
+++ controllers/api.js
@@ -7,25 +7,19 @@ const dest = '/home/wwwroot/default/gost-cli/version'
 const full_name = 'gost-c/gost-cli'
 
 exports.index = async ctx => {
-  debug(ctx.request.body)
-
   payload = ctx.request.body
 
   if (payload.action === 'published' && !payload.release.draft && !payload.release.prerelease) {
+    debug(ctx.request.body)
     const version = payload.release.tag_name
-    try {
-      await execa('sh', ['-c', sh])
-      fs.writeFileSync(dest, version)
-      return ctx.body = {
-        status: 'ok',
-        msg: 'all done'
-      }
-    } catch (err) {
-      console.log(err)
-      return ctx.body = {
-        status: 'error',
-        msg: err
-      }
+    // not wait
+    execa('sh', ['-c', sh])
+    fs.writeFileSync(dest, version)
+
+    return ctx.body = {
+      status: 'ok',
+      msg: `buding version ${version}`,
+      checkUrl: 'http://congz.pw/gost-cli/version'
     }
   }
 
